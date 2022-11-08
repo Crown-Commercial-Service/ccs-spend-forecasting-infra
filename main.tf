995f26667ab4cef1718b405ccd573ff8824e3787
Charlie Wren <cwren92@gmail.com>
2022-11-08 11:08:00 +0000

    Initial backend config

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-ccs-tfstate"
+    storage_account_name = "ccstfstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}

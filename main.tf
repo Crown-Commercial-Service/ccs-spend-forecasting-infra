f3165b06f09431a1f32b541d28a47370e13bb50a
Joe Fong <joe.fong@madetech.com>
2022-11-08 11:30:29 +0000

    add databricks

--- main.tf
+++ main.tf
@@ -32,3 +32,14 @@ resource "azurerm_data_factory" "import_factory" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
+
+resource "azurerm_databricks_workspace" "databricks" {
+  name                = "databricks-test"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "standard"
+
+  tags = {
+    ManagedBy = "Terraform"
+  }
+}

65564a8219ffa980ffd15871e728396e3b61930b
Charlie Wren <cwren92@gmail.com>
2022-12-08 16:55:20 +0000

    Remove commented code

--- pipeline/databricks.tf
+++ pipeline/databricks.tf
@@ -1,9 +1,3 @@
-# module "databricks_workspace" {
-#   source              = "../modules/databricks_workspace"
-#   name                = "${local.resource_group_name}-${var.stack_identifier}-databricks"
-#   resource_group_name = local.resource_group_name
-# }
-
 resource "azurerm_databricks_workspace" "databricks" {
   name                = "${local.resource_group_name}-${var.stack_identifier}-databricks"
   resource_group_name = data.terraform_remote_state.bootstrap.outputs.resource_group_name
